3d6eaa8e99ad0c4846d6d9e6037ecb94af2c5c01
Pavel Petrov <ppetrov@mirantis.com>
2017-11-18 20:29:07 +0200

    Выделена общая фикстура в conftest.py

--- fixture/group.py
+++ fixture/group.py
@@ -20,4 +20,13 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
-        self.app.navigation.return_to_group_page()
+        self.app.navigation.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.app.navigation.open_group_page()
+        # select group to delete
+        wd.find_element_by_name("selected[]").click()
+        # submit group deletion
+        wd.find_element_by_name("delete").click()
+        self.app.navigation.return_to_groups_page()
--- fixture/navigation.py
+++ fixture/navigation.py
@@ -16,6 +16,6 @@ class NavigationHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
-    def return_to_group_page(self):
+    def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()

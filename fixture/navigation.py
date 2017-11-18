1679c93b784c88b518c3455b739895ba448e1d4a
Pavel Petrov <ppetrov@mirantis.com>
2017-11-18 20:01:56 +0200

    Выделены классы помощники для групп, контактов, сессии и навигации

---
+++ fixture/navigation.py
@@ -0,0 +1,21 @@
+
+
+class NavigationHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def open_home_page(self):
+        wd = self.app.wd
+        wd.get("http://localhost/addressbook/index.php")
+
+    def go_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+
+    def open_group_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def return_to_group_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,32 +14,32 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(fname="test fname", mname="test mname",
-                                lname="test lname", nickname="test nickname",
-                                title="test title", company="test company",
-                                primary_address="test address", primary_address_phone="111",
-                                mobile_phone="222", work_home="333", fax="444",
-                                email_1="testemail1@test.test", email_2="testemail2@test.test",
-                                email_3="testemail3@test.test", homepage="homepage.home",
-                                birthday_day="5", birthday_month="January",
-                                birthday_year="1969", anniversary_day="5",
-                                anniversary_month="February", anniversary_year="2009",
-                                secondary_address="test address2", secondary_address_phone="555",
-                                notes="note note note note note note note note note note"
+    app.contact.create(Contact(fname="test fname", mname="test mname",
+                               lname="test lname", nickname="test nickname",
+                               title="test title", company="test company",
+                               primary_address="test address", primary_address_phone="111",
+                               mobile_phone="222", work_home="333", fax="444",
+                               email_1="testemail1@test.test", email_2="testemail2@test.test",
+                               email_3="testemail3@test.test", homepage="homepage.home",
+                               birthday_day="5", birthday_month="January",
+                               birthday_year="1969", anniversary_day="5",
+                               anniversary_month="February", anniversary_year="2009",
+                               secondary_address="test address2", secondary_address_phone="555",
+                               notes="note note note note note note note note note note"
                                       "note note note note note note note note note note note note"))
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(fname="", mname="", lname="", nickname="",
-                                title="", company="", primary_address="",
-                                primary_address_phone="", mobile_phone="",
-                                work_home="", fax="",  email_1="", email_2="",
-                                email_3="", homepage="", birthday_day="",
-                                birthday_month="-", birthday_year="",
-                                anniversary_day="", anniversary_month="-",
-                                anniversary_year="", secondary_address="",
-                                secondary_address_phone="", notes=""))
+    app.contact.create(Contact(fname="", mname="", lname="", nickname="",
+                               title="", company="", primary_address="",
+                               primary_address_phone="", mobile_phone="",
+                               work_home="", fax="", email_1="", email_2="",
+                               email_3="", homepage="", birthday_day="",
+                               birthday_month="-", birthday_year="",
+                               anniversary_day="", anniversary_month="-",
+                               anniversary_year="", secondary_address="",
+                               secondary_address_phone="", notes=""))
 
     app.session.logout()

a8545ff7d78b08d3f58eb7ca24ccfd8072577595
Pavel Petrov <ppetrov@mirantis.com>
2017-12-22 21:11:53 +0200

    Промежуточное сохранение новой фикстуры

---
+++ fixture/orm.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+from pony.orm import *
+from pymysql.converters import encoders, decoders, convert_mysql_timestamp
+
+from model.group import Group
+from model.contact import Contact
+
+
+class ORMFixture:
+
+    db = Database()
+
+    class ORMGroup(db.Entity):
+        _table_ = 'group_list'
+        id = PrimaryKey(int, column='group_id')
+        name = Optional(str, column='group_name')
+        header = Optional(str, column='group_header')
+        footer = Optional(str, column='group_footer')
+        # 'lazy'-option extracts data from DB only when a reference to corresponding field occurs.
+        # This prevents DB from serving too huge load when populating large data-structures
+        # with a lot of table references/cross-references.
+        contacts = Set(lambda: ORMFixture.ORMContact,
+                       table='address_in_groups', column='id', reverse='groups', lazy=True)
+
+    class ORMContact(db.Entity):
+        _table_ = 'addressbook'
+        id = PrimaryKey(int, column='id')
+        firstname = Optional(str, column='firstname')
+        lastname = Optional(str, column='lastname')
+        deprecated = Optional(str, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup,
+                     table='address_in_groups', column='group_id', reverse='contacts', lazy=True)
+
+    def __init__(self, host, name, user, password):
+        conv = encoders
+        conv.update(decoders)
+        conv[datetime] = convert_mysql_timestamp
+        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=conv)
+        self.db.generate_mapping()
+        sql_debug(True)
+
+    def convert_db_groups_to_model_groups(self, groups):
+        def convert(db_obj):
+            return Group(id=str(db_obj.id), name=db_obj.name, header=db_obj.header, footer=db_obj.footer)
+        return list(map(convert, groups))
+
+    def convert_db_contacts_to_model_contacts(self, groups):
+        def convert(db_obj):
+            return Contact(id=str(db_obj.id), fname=db_obj.firstname, lname=db_obj.lastname)
+        return list(map(convert, groups))
+
+    @db_session
+    def get_group_list(self):
+        return self.convert_db_groups_to_model_groups(list(select(g for g in ORMFixture.ORMGroup)))
+
+    @db_session
+    def get_contact_list(self):
+        return self.convert_db_contacts_to_model_contacts(list(select(c for c in ORMFixture.ORMContact
+                                                                      if c.deprecated is None)))
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_db_contacts_to_model_contacts(orm_group.contacts)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+
+        return self.convert_db_contacts_to_model_contacts(
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
+
+    # def get_group_list(self):
+    #     with db_session:
+    #         return list(select(g for g in ORMFixture.ORMGroup))

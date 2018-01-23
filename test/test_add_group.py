43e10c0ea7bb57873880860e4fcb9a07acff3077
Pavel Petrov <ppetrov@mirantis.com>
2018-01-23 22:21:55 +0200

    ДЗ29: тест для добавления группы модифицирован для возможности построения Allure-отчётов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
+import pytest
 
 from model.group import Group
 from utils.data_transformations import produce_instance_for_groups_page_view
 
 
 def test_add_group(app, db, check_ui, json_groups):
-    old_groups = db.get_group_list()
+    with pytest.allure.step('Given a group list'):
+        old_groups = db.get_group_list()
+
     group = json_groups
-    app.groups.create(group)
+    with pytest.allure.step('When I add a group %s to the list' % group):
+        app.groups.create(group)
 
-    if check_ui:
-        assert len(old_groups) + 1 == app.groups.count()
+    with pytest.allure.step('Then the new group list is equal to the old group list (with the added group)'):
+        if check_ui:
+            assert len(old_groups) + 1 == app.groups.count()
 
-    new_groups = db.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_maxval) == sorted(new_groups, key=Group.id_or_maxval)
+        new_groups = db.get_group_list()
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_maxval) == sorted(new_groups, key=Group.id_or_maxval)
 
-    if check_ui:
-        db_groups_list = list(map(produce_instance_for_groups_page_view, new_groups))
-        ui_groups_list = app.groups.get_group_list()
-        assert sorted(db_groups_list, key=Group.id_or_maxval) == sorted(ui_groups_list, key=Group.id_or_maxval)
+        if check_ui:
+            db_groups_list = list(map(produce_instance_for_groups_page_view, new_groups))
+            ui_groups_list = app.groups.get_group_list()
+            assert sorted(db_groups_list, key=Group.id_or_maxval) == sorted(ui_groups_list, key=Group.id_or_maxval)
